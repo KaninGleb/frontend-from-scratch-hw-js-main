9b5ccae3d0864ff9b478013dfbbe8a11d1a032fa
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-18 01:20:09 +0300

    Lesson-02/01

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -5,14 +5,23 @@
 // * - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
 // * - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
 
+// True:
+// const isAdmin = false;
+// const isVerifiedUser = true;
+// const hasSpecialPermission = true;
+// const hasTemporaryPass = false;
+
+// False:
 const isAdmin = false;
 const isVerifiedUser = true;
-const hasSpecialPermission = true;
+const hasSpecialPermission = false;
 const hasTemporaryPass = false;
 
 // Solution:
-let isAccess = true;
+// let isAccess = true;
+let isAccess = false;
 
 let giveMeAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass);
 
 console.log(giveMeAccess);
+
