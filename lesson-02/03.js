b9ef733348ffbab67cee928dae85cf1e9e1f5473
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-18 17:47:13 +0300

    Lesson-02/03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,25 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
-// your code
+// const score = Math.floor(Math.random() * 101); 
+// console.log(score);
+
+const score = 70;
+// let grade
+
+if (score >= 0 && score <= 49) {
+    grade = "F";
+    console.log(grade);
+} else if (score >= 50 && score <= 69) {
+    grade = "D"
+    console.log(grade);
+} else if (score >= 70 && score <= 79) {
+    grade = "C"
+    console.log(grade);
+} else if (score >= 80 && score <= 89) {
+    grade = "B"
+    console.log(grade);
+} else {
+    grade = "A"
+    console.log(grade);
+}
