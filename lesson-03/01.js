7cc93c7ab693313351f0f2e94a15d3543c8b8208
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-22 20:51:24 +0300

    Lesson-03/01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,11 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+let num_1 = Math.ceil(Math.random() * 10);
+let num_2 = Math.ceil(Math.random() * 10);
+
+function calculateAverage(number1, number2) {
+    return(number1 + number2) / 2;
+}
+
+console.log(`Numbers: ${num_1}, ${num_2}.`);
+console.log(`Average: ${calculateAverage(num_1, num_2)}.`);
