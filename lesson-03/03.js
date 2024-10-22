93f98bb0d5ab6fba7fbbcdbd5302a8177df0a19b
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-22 20:34:19 +0300

    Lesson-03/03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+let num_1 = Math.ceil(Math.random() * 3);
+let num_2 = Math.ceil(Math.random() * 3);
+let num_3 = Math.ceil(Math.random() * 3);
 
-function findLargest() {}
+
+function findLargest(number1, number2, number3) {
+    return(Math.max(number1, number2, number3))
+}
+
+console.log(num_1, num_2, num_3);
+console.log(`🥇 Winner: ${findLargest(num_1, num_2, num_3)}`);
