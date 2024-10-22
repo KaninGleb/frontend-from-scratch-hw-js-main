b1c89381426a92d67a1ba9f6562e68ffca508848
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-22 21:11:05 +0300

    Lesson-03/02

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,23 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+const num_1 = Math.ceil(Math.random() * 10);
+
+// // If...else
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// Ternary operator
+function isEven(number) {
+    const result = number % 2 === 0 ? true : false;
+    return result;
+}
+
+
+console.log(`Number: ${num_1}.`);
+console.log(`Is the number even? - ${isEven(num_1)}`);
