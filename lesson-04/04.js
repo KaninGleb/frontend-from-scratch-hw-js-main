d4381e03634f4a2059edbd60dc16c568638b7a26
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-27 20:22:48 +0300

    Lesson-04/04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -1,15 +1,19 @@
-/*
-Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
+// Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
 
-Входные данные:
+// Входные данные:
+// - **`str`**: Строка, символы которой нужно удвоить.
 
-- **`str`**: Строка, символы которой нужно удвоить.
+// Выходные данные:
+// - Новая строка с каждым символом, повторенным дважды.
 
-Выходные данные:
+// Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 
-- Новая строка с каждым символом, повторенным дважды.
+function doubleEachCharacter(str) {
+    let doubleString = "";
+    for (let i = 0; i < str.length; i++) {
+        doubleString += str[i].repeat(2);
+    }
+    return doubleString;
+}
 
-Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
-*/
-
-function doubleEachCharacter() {}
+console.log(doubleEachCharacter('hello'));
