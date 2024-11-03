126551d4486b5d4c0e5d00ae083453ef71d6f653
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-03 21:57:26 +0300

    Lesson-05/02

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,33 +1,29 @@
-/*
-Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
+// Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
+// - объект галереи
+// - название произведения (ключ)
+// - новое значение
+// Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
 
-- объект галереи
-- название произведения (ключ)
-- новое значение
+// Пример использования функции:
 
-Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
+// const gallery = {
+//   'Mona Lisa': 'Leonardo da Vinci',
+//   'Starry Night': 'Vincent van Gogh',
+//   'The Scream': 'Edvard Munch'
+// }
 
-Пример использования функции:
+// updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+// updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
 
-const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch'
-}
+// console.log(gallery)
 
-updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
-updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
-
-console.log(gallery)
-
-Ожидаемый вывод:
-{
-  'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-  'The Persistence of Memory': 'Salvador Dali'
-}
-*/
+// Ожидаемый вывод:
+// {
+//   'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
+//   'Starry Night': 'Vincent van Gogh',
+//   'The Scream': 'Edvard Munch',
+//   'The Persistence of Memory': 'Salvador Dali'
+// }
 
 const gallery = {
   'Mona Lisa': 'Leonardo da Vinci',
@@ -35,4 +31,15 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, artName, newValue) {
+  if (gallery.hasOwnProperty(artName)) {
+    gallery[artName] = newValue;
+  } else {
+    gallery[artName] = newValue;
+  }
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali');
+
+console.log(gallery);
