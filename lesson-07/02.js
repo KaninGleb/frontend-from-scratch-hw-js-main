3df4527a954b226896217493e2f325dcf54e1943
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-16 21:54:28 +0300

    Lesson-07/02

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -1,14 +1,31 @@
-/*
- * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
- * Если строка является числом, функция должна возвращать true, в противном случае - false.
- */
+//  * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
+//  * Если строка является числом, функция должна возвращать true, в противном случае - false.
 
-function isNumeric(str) {
-  // your code
-}
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+// ✅ SOLUTION
+// OPTION 1
+// function isNumeric(str) {
+//   if (Number(str)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+// OPTION 2
+// function isNumeric(str) {
+//   return Number(str) ? true : false;
+// }
+
+
+// OPTION 3
+const isNumeric = str => Number(str) ? true : false;
+
+
+// ✅ Checking
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
