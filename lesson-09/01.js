363ef636bb37d7219d9e0bc7c02f6c2e3dd7a323
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-29 21:36:16 +0300

    Lesson-09/01

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -1,26 +1,46 @@
-/*
-Напишите свою версию метода `filter`.
-Ваша задача — создать функцию `filter`, которая принимает два параметра:
+// ❓DESCRIPTION
+// Напишите свою версию метода `filter`.
+// Ваша задача — создать функцию `filter`, которая принимает два параметра:
+//
+// - `array` (массив, над которым выполняется операция),
+// - `callback` (функция, которая вызывается для каждого элемента массива).
+//
+// Функция `callback` должна принимать два параметра:
+//
+// - `element` (текущий элемент массива)
+// - `index` (индекс текущего элемента)
+//
+// Функция `filter` должна возвращать новый массив, содержащий только те элементы `array`, для которых `callback` возвращает `true`.
 
-- `array` (массив, над которым выполняется операция),
-- `callback` (функция, которая вызывается для каждого элемента массива).
 
-Функция `callback` должна принимать два параметра:
+// Пример использования:
+// const numbers = [1, 2, 3, 4, 5]
+//
+// const oddNumbers = filter(numbers, (element, index) => {
+//     return element % 2 !== 0
+// });
+//
+// console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 
-- `element` (текущий элемент массива)
-- `index` (индекс текущего элемента)
 
-Функция `filter` должна возвращать новый массив, содержащий только те элементы `array`, для которых `callback` возвращает `true`.
-*/
+// ✅ SOLUTION
+const filter = (array, callback) => {
+    const result = [];
 
-/* Пример использования:
+    array.forEach((el, index) => {
+        if (callback(el, index)) {
+            result.push(el);
+        }
+    })
+    return result;
+}
+
+
+// ✅ Checking
 const numbers = [1, 2, 3, 4, 5]
 
 const oddNumbers = filter(numbers, (element, index) => {
-  return element % 2 !== 0
+    return element % 2 !== 0
 });
 
-console.log(oddNumbers) // Должен вывести: [1, 3, 5]
-*/
-
-const filter = () => {}
+console.log(oddNumbers);
